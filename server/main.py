81dcf18881b95041df4a43e09baf04a72b8c70ea
Abhilasha <singhabhilasha4042@gmail.com>
2025-03-20 23:36:43 +0530

    fix port

--- server/main.py
+++ server/main.py
@@ -81,7 +81,7 @@ async def health_check():
     return {"version": settings.VERSION, "status": "healthy"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 """
